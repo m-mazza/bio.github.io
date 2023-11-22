82ab23dca0522ec9052c3d946b7f60c7878ba2e3
Marcus Mazza <m-mazza@live.com>
2023-11-22 17:26:19 -0300

    incluindo accordian e animando incone de expansão

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,18 @@ $(function() {
 
     var banner = $('.banner-content').data('bg');
     $('.banner-content').css('background-image', 'url('+ banner +')');
-
         
+
+    $('#rotina-trabalho p').on('click', function() {
+
+        if( $(this).find('i').hasClass('anima') ) {
+            $(this).find('i').toggleClass('anima-back'); 
+        } else {
+            $(this).find('i').toggleClass('anima');   
+        }
+     
+    });
     
+
+
 })
