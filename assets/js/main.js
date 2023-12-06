12e450ddabd7080d95798f843582314dd7532fdb
Marcus Mazza <m-mazza@live.com>
2023-12-05 23:47:40 -0300

    princípio da área navbar mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 $(function() {
 
-
+    // BANNER - cria o data-bg, que capta a imagem direto html
     var banner = $('.banner-content').data('bg');
     $('.banner-content').css('background-image', 'url('+ banner +')');
         
 
+    //EXPERIÊNCIA PROFISSIONAL - anima a seta do collapse
     $('#rotina-trabalho p').on('click', function() {
 
         if( $(this).find('i').hasClass('anima') ) {
@@ -16,5 +17,16 @@ $(function() {
     });
     
 
+    // NAVBAR MOBILE
+    $('.mobile-trigger i').on('click', function(e) {
+        $('.mobile-menu-wrapper').css('transform', 'translate(0%)');
+        if(e.target.className != 'fa-solid fa-bars'){
+            $('.mobile-menu-wrapper').css('transform', 'translate(-100%)');
+        }
+    });
+
+    $(window).resize(function() {
+        $('.mobile-menu-wrapper').css('transform', 'translate(-100%)');
+    })
 
 })
